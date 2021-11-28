1158e3151e724d0ac3fce70688bb4c8acf5b4a4e
Camilo Morales <camilo.morales@itv.com>
2021-11-28 20:25:45 +0000

    Just some rogue type class definitions in Main

--- exercises/chapter3/src/Main.purs
+++ exercises/chapter3/src/Main.purs
@@ -5,6 +5,12 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+example :: Int -> Int -> Int -> Int
+example x y z = foo + bar
+  where
+    foo = x * y
+    bar = y * z
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ show $ example 10 2 3
--- exercises/chapter6/src/Main.purs
+++ exercises/chapter6/src/Main.purs
@@ -1,4 +1,7 @@
-module Main where
+module Main
+  ( main
+  )
+  where
 
 import Prelude
 
@@ -6,12 +9,29 @@ import Effect (Effect)
 import Effect.Class.Console (logShow)
 import Data.Hashable (hash, hashEqual)
 
+newtype MyBool = MyBool Boolean
+
+data MyADTPets 
+  = Cat String Int
+  | Dog String Int
+
+instance myShowBoolean :: Show MyBool where
+  show (MyBool true) = "True oh yeah!"
+  show (MyBool false) = "Flase oh No!"
+
+instance myShowPets :: Show MyADTPets where
+  show (Cat name age) = "I'm a cat, my name is " <> name <> "and my age" <> show age
+  show (Dog name age) = "I'm a dog, my name is " <> name <> "and my age" <> show age
+
 main :: Effect Unit
 main = do
   logShow $ hash 123
   logShow (hash true)
-  logShow (hash [1, 2, 3])
+  logShow (hash [1, 2, 3, 4])
   logShow (hash "testing")
   logShow (hash 'a')
   logShow ("foo" `hashEqual` "foo")
   logShow ("foo" `hashEqual` "bar")
+  logShow (MyBool true)
+  logShow (Cat "Anastasia" 3)
+
